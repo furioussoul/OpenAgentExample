39e387b386facf1259da68f2ac05f41fb4a9fea6
geralt.sun <geralt.sun@ly.com>
2026-02-15 22:26:43 +0800

    添加 .gitignore 文件，创建示例代码和 package.json 配置

---
+++ example/simple.ts
@@ -0,0 +1,18 @@
+import { google, kimi, OpenAgent } from 'mycto_agent'
+
+// With proxy/custom endpoint
+// const agent = new OpenAgent({
+//   provider: 'anthropic',
+//   apiKey: 'your_api_key',
+//   baseURL: 'your_custom_endpoint',
+//   model: 'model_name',
+// })
+const agent = kimi("process.env.KIMI_API_KEY", 'kimi-k2.5-free')
+
+
+// const agent = google('your_api_key', 'model_name') // With Google provider
+
+const result = await agent.chat('帮我在当前目录创建一个10个文件，内容写上随机数字，然后再并行读取计算和')
+
+console.log(result.text)
+console.log(result.toolCalls) // 看看它调用了什么工具
